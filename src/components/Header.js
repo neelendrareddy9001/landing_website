d74124e6978812ca3c8f08b770f5ab40ce7393a3
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-11-21 21:00:43 +0100

    menu bar updated with styles mui icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,56 @@
+import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
 import React from "react";
+import FastfoodIcon from "@mui/icons-material/Fastfood";
+import { Link } from "react-router-dom";
+import ".././Styles/HeaderStyles.css";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <>
+      <Box>
+        <AppBar component={"nav"} sx={{ bgcolor: "black" }}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              sx={{
+                mr: 2,
+                display: { sm: "none" },
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              color={"goldenrod"}
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1 }}
+            >
+              <FastfoodIcon />
+              My Restaurent
+            </Typography>
+            <Box sx={{ display: { xs: "none", sm: "block" } }}>
+              <ul className="navigation-menu">
+                <li>
+                  <Link to={"/"}>Home</Link>
+                </li>
+                <li>
+                  <Link to={"/menu"}>Menu</Link>
+                </li>
+                <li>
+                  <Link to={"/about"}>About</Link>
+                </li>
+                <li>
+                  <Link to={"/contact"}>Contact</Link>
+                </li>
+              </ul>
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
   );
 };
 
