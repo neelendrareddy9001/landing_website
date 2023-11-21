4c4e24991a93a1675c88d1cc7489411a213bdc02
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-11-21 20:28:38 +0100

    first commit

---
+++ src/Pages/Menu.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const Menu = () => {
+  return (
+    <>
+      <Layout>
+        <h1>Menu</h1>
+      </Layout>
+    </>
+  );
+};
+
+export default Menu;
